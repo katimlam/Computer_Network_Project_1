c0fdb1858227a097a2962b971992a6c477aedc5e
wenhui kuang <wenhuikuang@wenhuis-MacBook-Air.local>
2016-10-15 16:58:28 -0700

    sss

---
+++ HttpMessage.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hhh
+//
+//  Created by wenhui kuang on 10/15/16.
+//  Copyright (c) 2016 wenhui kuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
