cdaf02a3935b22152b438a0d2deacbd5c4902f9f
katimlam <katimlam@yahoo.com>
2016-10-22 15:16:38 -0700

    webserver modified

--- web-server.cpp
+++ web-server.cpp
@@ -15,7 +15,6 @@
 #include <thread>
 using namespace std;
 
-#define BUF_SIZE 512;
 #define DEBUG 1;
 
 int
@@ -96,23 +95,26 @@ main(int argc, const char* argv[])
 		return 2;
 	}
 	
-	//////////////////////////////////////////////////////////////////////////////////
 	
 	// set socket to listen status
-	if (listen(sockfd, 1) == -1) {
+	if (listen(sockfd, 64) == -1) {
 		perror("listen");
 		return 3;
 	}
-
+	
+	//////////////////////////////////////////////////////////////////////////////////
 	// accept a new connection
+	while(1){
+		
+	}
 	struct sockaddr_in clientAddr;
 	socklen_t clientAddrSize = sizeof(clientAddr);
 	int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
 
-  if (clientSockfd == -1) {
-    perror("accept");
-    return 4;
-  }
+	if (clientSockfd == -1) {
+		perror("accept");
+		return 4;
+	}
 
   char ipstr[INET_ADDRSTRLEN] = {'\0'};
   inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
@@ -151,54 +153,3 @@ main(int argc, const char* argv[])
 
   return 0;
 }
-
-/*
-#include <iostream>
-#include <thread>
-#include <chrono>
-
-using namespace std;
-
-void
-countdown(int id, int count)
-{
-  while (count > 0) {
-    cout << "thread " << id << " is still alive." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    count--;
-  }
-  cout << "thread " << id << " is about to terminate." << std::endl;
-}
-
-void
-noJoin()
-{
-  for (int i = 0; i < 3; i++) {
-    thread(countdown, i+1, (i+1)*5).detach();
-    // thread t(countdown, i+1, (i+1)*5); // Wrong! thread is not safely destroyed.
-  }
-  std::this_thread::sleep_for(std::chrono::seconds(20));
-  cout << "main thread is about to terminate." << std::endl;
-}
-
-void
-join()
-{
-  thread t[3];
-  for (int i = 0; i < 3; i++) {
-    t[i] = thread(countdown, i+1, (i+1)*5);
-  }
-
-  for (int i = 0; i < 3; i++) { // must join, otherwise threads will not be released safely.
-    t[i].join();
-  }
-  cout << "main thread is about to terminate." << std::endl;
-}
-
-int main(int argc, char *argv[])
-{
-  noJoin();
-  // join();
-  return 0;
-}
-*/
